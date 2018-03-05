209211c6c1e807f72ab336ee2ffb0b015990e141
solange <solange.mendy@gmail.com>
2018-03-05 10:48:38 +0100

    add allocation to stagiaire model attribute

--- factory-angular/src/app/model/stagiaire.model.ts
+++ factory-angular/src/app/model/stagiaire.model.ts
@@ -5,19 +5,13 @@ export class Stagiaire {
   public id: number;
   public nom: string;
   public prenom: string;
-<<<<<<< HEAD:factory-angular/src/app/model/stagiaire.model.ts
   public dateNaissance:string;
   public adresse:string;
   public numTel:string;
   public email:string;
   public allocations: Array<Allocation> = [];
 
-=======
-  public dateNaissance: string;
-  public adresse: string;
-  public numTel: string;
-  public email: string;
->>>>>>> master:factory-angular/src/app/model/stagiaire.ts
+
 
   constructor() { }
 }
